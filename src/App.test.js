42e0f7269b779cc863dbddb54b5f92469810f7d8
KolesnikovMS <m.kolesnikov@id-mt.ru>
2022-05-13 14:31:12 +0300

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+import { MemoryRouter as Router } from 'react-router-dom'
 import App from './App'
 
 test('renders login page', () => {
-  render(<MemoryRouter><App /></MemoryRouter>)
+  render(
+    <RecoilRoot>
+      <Router history={history}>
+        <App />
+      </Router>
+    </RecoilRoot>
+  )
   const buttonElement = screen.getByText(/Вход/i)
   expect(buttonElement).toBeInTheDocument
 })
