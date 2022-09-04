6b306a7d7e4ce5cea013ed0de22a9955ddb37205
veronikageidt <prokopchuk_veron@mail.ru>
2022-09-04 10:56:12 +0500

    ASPD-151 tab history

--- src/Pages/Tasks/item/Pages/History/index.js
+++ src/Pages/Tasks/item/Pages/History/index.js
@@ -2,58 +2,69 @@ import React, {useContext, useEffect, useMemo, useState} from 'react';
 import PropTypes from 'prop-types';
 import BaseCell, {sizes as baseCellSize} from "../../../../../Components/ListTableComponents/BaseCell";
 import Select from "../../../../../Components/Inputs/Select";
-import {SearchInput} from "../../../list/styles";
+import {FilterForm, TableActionButton} from "../../styles";
 import DatePicker from "../../../../../Components/Inputs/DatePicker";
+import DatePickerComponent from '@Components/Components/Inputs/DatePicker'
 import {useParams} from "react-router-dom";
 import useTabItem from "../../../../../components_ocean/Logic/Tab/TabItem";
 import {ApiContext, TASK_ITEM_DOCUMENT, TASK_ITEM_HISTORY, TASK_ITEM_REQUISITES} from "../../../../../contants";
 import RowComponent from "../../../list/Components/RowComponent";
 import HeaderCell from "../../../../../Components/ListTableComponents/HeaderCell";
 import ListTable from "../../../../../components_ocean/Components/Tables/ListTable";
+import Icon from "../../../../../components_ocean/Components/Icon";
+import searchIcon from "../../../../../Icons/searchIcon";
+import SortCellComponent from "../../../../../Components/ListTableComponents/SortCellComponent";
+
+const plugins = {
+    outerSortPlugin: {component: SortCellComponent}
+}
 
 const columns = [
     {
         id: "event",
         label: "Событие",
-        component: <BaseCell/>,
-        sizes: baseCellSize
+        component: ({ParentValue: {eventLabel}}) => <BaseCell value={eventLabel} className="items-center"/>,
+        sizes: 200
     },
     {
         id: "loop",
         label: "Внейшний цикл",
-        component: <BaseCell/>,
+        component: ({ParentValue: {stageIteration}}) => <BaseCell value={stageIteration?.toString()  || ""}/>,
         sizes: baseCellSize
     },
     {
-        id: "loop",
+        id: "state",
         label: "Состояние",
-        component: <BaseCell/>,
+        component: ({ParentValue: {eventStatus}}) => <BaseCell value={eventStatus}/>,
         sizes: baseCellSize
     },
     {
-        id: "loop",
+        id: "executor",
         label: "Исполнитель",
-        component: <BaseCell/>,
+        component: ({ParentValue: {performer: {lastName = "", firstName, middleName}}}) => {
+            const fio = `${lastName} ${firstName && `${firstName[0]}.` || ""} ${middleName && `${middleName[0]}.` || ""}`
+            return <BaseCell value={fio} className="flex items-center h-full"/>
+        },
         sizes: baseCellSize
     },
     {
-        id: "loop",
+        id: "date",
         label: "Дата получения",
-        component: <BaseCell/>,
+        component: ({ParentValue: {eventDate}}) => <BaseCell value={eventDate}/>,
         sizes: baseCellSize
     },
     {
         id: "description",
         label: "Описание",
-        component: <BaseCell/>,
-        sizes: baseCellSize
+        component: ({ParentValue: {description}}) => <BaseCell value={description}/>,
+        sizes: 540
     }
 ]
 
 const filterFormConfig = [
     {
         id: "1",
-        component: DatePicker,
+        component: DatePickerComponent,
         placeholder: "Дата события",
     },
     {
@@ -74,7 +85,7 @@ const filterFormConfig = [
     {
         id: "3",
         component: Select,
-        placeholder: "Этап",
+        placeholder: "Исполнитель",
         options: [
             {
                 ID: "ASD",
@@ -106,27 +117,37 @@ const History = props => {
         setTabState({data: content})
     }, [api, setTabState, id])
 
-    // console.log(data, 'data')
-
+    const emptyWrapper = (({children}) => children)
+    const [a, b] = useState({})
 
   return (
     <div className="px-4 pb-4 overflow-hidden flex-container">
-      <div className="flex items-center">
-          <ListTable
-              // rowComponent={useMemo(() => (props) => <RowComponent
-              //     onDoubleClick={handleDoubleClick} {...props}
-              // />, [handleDoubleClick])}
-              value={data}
-              columns={columns}
-              // plugins={pluginslugins}
-              headerCellComponent={HeaderCell}
-              selectState={selectState}
-              onSelect={setSelectState}
-              sortQuery={sortQuery}
-              onSort={onSort}
-              valueKey="id"
-          />
+      <div className="flex items-center py-4">
+          <FilterForm
+              fields={filterFormConfig}
+              inputWrapper={emptyWrapper}
+              value={a}
+              onInput={b}
+          >
+          </FilterForm>
+          <TableActionButton className="ml-2 color-white bg-blue-2">
+              <Icon icon={searchIcon}/>
+          </TableActionButton>
       </div>
+        <ListTable
+            rowComponent={useMemo(() => (props) => <RowComponent
+                onDoubleClick={()=>null} {...props}
+            />, [])}
+            value={data}
+            columns={columns}
+            plugins={plugins}
+            headerCellComponent={HeaderCell}
+            selectState={selectState}
+            onSelect={setSelectState}
+            sortQuery={sortQuery}
+            onSort={onSort}
+            valueKey="id"
+        />
     </div>
   );
 };
---
+++ src/Pages/Tasks/item/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Form from '@Components/Components/Forms'
+import Input from '@Components/Components/Inputs/Input'
+
+export const FilterForm = styled(Form)`
+  --form--elements_height: 32px;
+  display: grid;
+  grid-template-columns: 200px 200px 200px;
+  grid-column-gap: 0.5rem;
+  `
+
+export const TableActionButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--light-gray);
+  width: 32px;
+  height: 32px;
+  border: 1px solid var(--separator);
+  box-sizing: border-box;
+  border-radius: 6px;
+  `
