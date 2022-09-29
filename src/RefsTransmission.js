5dbf6f7d10deecd0291adc10447901cc76472b06
KolesnikovMS <m.kolesnikov@id-mt.ru>
2022-09-29 19:05:24 +0300

    APSD-231 Титул как справочник

--- src/RefsTransmission.js
+++ src/RefsTransmission.js
@@ -2,5 +2,9 @@ export default {
   "ddt_document_status": {
     valueKey: "dss_name",
     labelKey: "dss_caption"
+  },
+  "ddt_startup_complex_type_doc": {
+    valueKey: "r_object_id",
+    labelKey: "dss_description"
   }
 }
