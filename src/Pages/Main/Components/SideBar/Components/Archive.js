b73142361cd9427cd4b8444a459c9840e28a522e
mac os <forinventorwork@mail.ru>
2022-10-14 23:08:58 +0500

    APSD - 298 список титулов

--- src/Pages/Main/Components/SideBar/Components/Archive.js
+++ src/Pages/Main/Components/SideBar/Components/Archive.js
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useMemo, useState } from 'react'
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import PropTypes from 'prop-types'
 import { NavigationHeaderIcon } from '../style'
 import ArchiveIcon from '../icons/ArchiveIcon'
@@ -6,8 +12,12 @@ import WithToggleNavigationItem from './withToggleNavigationItem'
 import angleIcon from '@/Icons/angleIcon'
 import Icon from '@Components/Components/Icon'
 import useTabItem from '@Components/Logic/Tab/TabItem'
-import { ApiContext, STORAGE, WINDOW_ADD_SUBSCRIPTION } from '@/contants'
-import { URL_STORAGE_LIST, URL_SUBSCRIPTION_EVENTS } from '@/ApiList'
+import { ApiContext } from '@/contants'
+import {
+  URL_STORAGE_BRANCH,
+  URL_STORAGE_TITLE,
+  URL_STORAGE_DOCUMENT,
+} from '@/ApiList'
 import { useParams } from 'react-router-dom'
 import { LOGIN_PAGE_PATH } from '@/routePaths'
 import ScrollBar from '@Components/Components/ScrollBar'
@@ -21,26 +31,17 @@ const Storage = (props) => {
 
   useEffect(() => {
     ;(async () => {
-      const { data } = await api.post(URL_STORAGE_LIST)
+      const { data } = await api.post(URL_STORAGE_BRANCH)
       setBranches(data)
     })()
   }, [api])
 
-  useEffect(() => {
-    ;(async () => {
-      const { data } = await api.post(URL_STORAGE_LIST)
-      setBranches(data)
-    })()
-  }, [api])
-
-  console.log(data, 'data')
-
-  const links = useMemo(
-    () => (
-      <>
-        {data.map(({ id, name }) => (
-          <WithToggleNavigationItem id={id} key={id}>
-            {({ isDisplayed, toggleDisplayedFlag }) => (
+  const renderArchiveItem = useCallback(
+    (url) =>
+      ({ id, name }) =>
+        (
+          <WithToggleNavigationItem id={id} key={id} url={url}>
+            {({ isDisplayed, toggleDisplayedFlag, child }) => (
               <div className="font-size-12 mt-2 pl-2">
                 <button
                   type="button"
@@ -56,13 +57,19 @@ const Storage = (props) => {
                     }`}
                   />
                 </button>
+                {isDisplayed && (
+                  <div className="flex flex-col pl-4">
+                    {Array.isArray(child)
+                      &&child?.map(renderArchiveItem(URL_STORAGE_DOCUMENT))
+                      // : child?.content?.map(renderArchiveItem(URL_STORAGE_DOCUMENT))
+                    }
+                  </div>
+                )}
               </div>
             )}
           </WithToggleNavigationItem>
-        ))}
-      </>
-    ),
-    [data],
+        ),
+    [],
   )
 
   return (
@@ -81,7 +88,11 @@ const Storage = (props) => {
               className={isDisplayed ? '' : 'rotate-180'}
             />
           </button>
-          {isDisplayed && <ScrollBar>{links}</ScrollBar>}
+          {isDisplayed && (
+            <ScrollBar>
+              {branches.map(renderArchiveItem(URL_STORAGE_TITLE))}
+            </ScrollBar>
+          )}
         </div>
       )}
     </WithToggleNavigationItem>
--- src/Pages/Main/Components/SideBar/Components/withToggleNavigationItem.js
+++ src/Pages/Main/Components/SideBar/Components/withToggleNavigationItem.js
@@ -1,17 +1,44 @@
 import PropTypes from 'prop-types'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { cachedLocalStorageValue } from '@Components/Logic/Storages/localStorageCache'
-import { useCallback } from 'react'
+import { useCallback, useContext, useEffect } from 'react'
+import {URL_STORAGE_DOCUMENT, URL_STORAGE_TITLE} from '@/ApiList'
+import { ApiContext } from '@/contants'
+import { documentQuery } from './store'
+import log from "tailwindcss/lib/util/log";
 
-const WithToggleNavigationItem = ({ id, children }) => {
+const keys = {
+  [URL_STORAGE_TITLE]: "branchId",
+  [URL_STORAGE_DOCUMENT]: "titleId"
+}
+
+const WithToggleNavigationItem = ({ id, children, url }) => {
+  const api = useContext(ApiContext)
   const [isDisplayed, setDisplayedFlag] = useRecoilState(
     cachedLocalStorageValue(`navigationItem${id}`),
   )
-  console.log(isDisplayed, 'isDisplayed')
+
+  const [child, setChild] = useRecoilState(documentQuery(id))
+
+  useEffect(() => {
+    !child && url &&
+      (async () => {
+        const { data } = await api.post(url, {
+          filter: { [keys[url]]: id },
+          limit: 10,
+          offset: 0,
+        })
+        console.log(data, "inc")
+        setChild(data)
+      })()
+  }, [child])
+
+  console.log(child, 'child')
 
   return children({
     isDisplayed,
     toggleDisplayedFlag: useCallback(() => setDisplayedFlag((s) => !s), []),
+    child
   })
 }
 
