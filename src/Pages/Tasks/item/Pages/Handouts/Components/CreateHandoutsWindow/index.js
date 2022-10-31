c47ffd2ab51b1f26eb333b8fe2218f3f45ab762a
mac os <forinventorwork@mail.ru>
2022-10-31 20:25:28 +0500

    APSD - 383

--- src/Pages/Tasks/item/Pages/Handouts/Components/CreateHandoutsWindow/index.js
+++ src/Pages/Tasks/item/Pages/Handouts/Components/CreateHandoutsWindow/index.js
@@ -14,59 +14,15 @@ import DatePickerComponent from '@Components/Components/Inputs/DatePicker'
 import UserSelect from '@/Components/Inputs/UserSelect'
 import BaseUserSelect from '@/Components/Inputs/OrgStructure/BaseUserSelect'
 import { SearchInput } from '@/Pages/Tasks/list/styles'
-import { URL_HANDOUTS_CREATE } from '@/ApiList'
-
-const fields = [
-  {
-    id: 'operationId',
-    component: LoadableSelect,
-    placeholder: 'Выберите операцию',
-    valueKey: 'dss_name',
-    labelKey: 'dss_name',
-    // loadFunction: async () => {
-    //   const { data } = await api.post(`${URL_ENTITY_LIST}/${TASK_TYPE}`)
-    //   return data
-    // },
-  },
-  {
-    id: 'operationDate',
-    component: DatePickerComponent,
-    placeholder: 'Выберите дату',
-  },
-  {
-    id: 'archivistId',
-    component: BaseUserSelect,
-    placeholder: 'Выберите архивариуса',
-  },
-  {
-    id: 'workerId',
-    component: UserSelect,
-    multiple: true,
-    placeholder: 'Выберите участников',
-  },
-  {
-    id: 'departmentId',
-    component: LoadableSelect,
-    placeholder: 'Выберите департамент',
-    valueKey: 'dss_name',
-    labelKey: 'dss_name',
-    // loadFunction: async () => {
-    //   const { data } = await api.post(`${URL_ENTITY_LIST}/${TASK_TYPE}`)
-    //   return data
-    // },
-  },
-  {
-    id: 'comment',
-    component: SearchInput,
-    placeholder: 'Введите комментарий',
-  },
-]
+import { URL_ENTITY_LIST, URL_HANDOUTS_CREATE } from '@/ApiList'
+import { TASK_TYPE } from '@/Pages/Tasks/list/constants'
 
 const CreateHandoutsWindow = (props) => {
   const api = useContext(ApiContext)
   const { id } = useParams()
   const [open, setOpenState] = useState(false)
   const [filterValue, setFilterValue] = useState({})
+  const [value, sendValue] = useState({})
 
   const changeModalState = useCallback(
     (nextState) => () => {
@@ -97,6 +53,58 @@ const CreateHandoutsWindow = (props) => {
     changeModalState(false)
   }, [changeModalState, filterValue, api])
 
+  const fields = useMemo(
+    () => [
+      {
+        id: 'operationId',
+        component: LoadableSelect,
+        placeholder: 'Выберите операцию',
+        valueKey: 'dss_name',
+        labelKey: 'dss_name',
+        loadFunction: async () => {
+          const { data } = await api.post(URL_ENTITY_LIST, {
+            type: 'ddt_dict_operations',
+          })
+          return data
+        },
+      },
+      {
+        id: 'operationDate',
+        component: DatePickerComponent,
+        placeholder: 'Выберите дату',
+      },
+      {
+        id: 'archivistId',
+        component: BaseUserSelect,
+        placeholder: 'Выберите архивариуса',
+      },
+      {
+        id: 'workerId',
+        component: (props) => <UserSelect {...props} sendValue={sendValue} />,
+        multiple: false,
+        returnObjects: true,
+        placeholder: 'Выберите участников',
+      },
+      {
+        id: 'departmentId',
+        component: LoadableSelect,
+        placeholder: 'Выберите департамент',
+        valueKey: 'dss_name',
+        labelKey: 'dss_name',
+        // loadFunction: async () => {
+        //   const { data } = await api.post(`${URL_ENTITY_LIST}/${TASK_TYPE}`)
+        //   return data
+        // },
+      },
+      {
+        id: 'comment',
+        component: SearchInput,
+        placeholder: 'Введите комментарий',
+      },
+    ],
+    [api, sendValue],
+  )
+
   return (
     <div className="flex items-center ml-auto ">
       <SecondaryBlueButton className="mr-2" onClick={changeModalState(true)}>
@@ -107,17 +115,19 @@ const CreateHandoutsWindow = (props) => {
         open={open}
         onClose={changeModalState(false)}
       >
-        <div className="flex py-4  w-full">
-          <TitlesContainer>{titles}</TitlesContainer>
-          <FilterForm
-            className="form-element-sizes-40"
-            fields={fields}
-            value={filterValue}
-            onInput={setFilterValue}
-            inputWrapper={EmptyInputWrapper}
-          />
+        <div className="flex flex-col overflow-hidden h-full">
+          <div className="flex py-4">
+            <TitlesContainer>{titles}</TitlesContainer>
+            <FilterForm
+              className="form-element-sizes-40"
+              fields={fields}
+              value={filterValue}
+              onInput={setFilterValue}
+              inputWrapper={EmptyInputWrapper}
+            />
+          </div>
         </div>
-        <div className="flex items-center justify-end">
+        <div className="flex items-center justify-end mt-8">
           <Button
             className="bg-light-gray flex items-center w-60 rounded-lg mr-4 font-weight-normal justify-center"
             onClick={changeModalState(false)}
