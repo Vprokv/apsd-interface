a4cd6a5e24e0cc653accce421933abbf3b36e5a4
KolesnikovMS <m.kolesnikov@id-mt.ru>
2022-11-03 11:13:15 +0300

    reverted DownloadFile.js, must be the same as it was in dev branch, it fails the build

--- src/Pages/Tasks/item/Pages/Content/Components/DownloadFile.js
+++ src/Pages/Tasks/item/Pages/Content/Components/DownloadFile.js
@@ -3,16 +3,18 @@ import { BoxForFile, GridForFiles } from '../style'
 import Button, { ButtonForIcon } from '@/Components/Button'
 
 const DownloadFile = () => {
-  /* <GridForFiles>*/
-  /*  <BoxForFile className="flex items-center justify-center">*/
-  /*    <Button*/
-  /*      className="text-white bg-blue-1 flex items-center w-52 rounded-lg justify-center font-weight-normal"*/
-  /*      onClick={() => null}*/
-  /*    >*/
-  /*      Загрузить контент*/
-  /*    </Button>*/
-  /*  </BoxForFile>*/
-  /* </GridForFiles>*/
+  return (
+    {/*<GridForFiles>*/}
+  {/*  <BoxForFile className="flex items-center justify-center">*/}
+  {/*    <Button*/}
+  {/*      className="text-white bg-blue-1 flex items-center w-52 rounded-lg justify-center font-weight-normal"*/}
+  {/*      onClick={() => null}*/}
+  {/*    >*/}
+  {/*      Загрузить контент*/}
+  {/*    </Button>*/}
+  {/*  </BoxForFile>*/}
+  {/*</GridForFiles>*/}
+  )
 }
 
 export default DownloadFile
