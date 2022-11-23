47e3f62836a9316ab96bd0a22f639f2be2c792f5
forinventorwork@mail.ru <y.prokopchuk@id-mt.ru>
2022-11-24 00:38:21 +0300

    APSD-463 APSD-517

--- src/Pages/Tasks/item/Hooks/useDocumentTabs.js
+++ src/Pages/Tasks/item/Hooks/useDocumentTabs.js
@@ -5,8 +5,8 @@ import { Route } from 'react-router-dom'
 const useDocumentTabs = (documentTabs, pages) =>
   useMemo(() => {
     if (!documentTabs) {
-      return { }
-      // return { headers: [], routes: [], defaultPath: pages[0].path }
+      // return { }
+      return { headers: [], routes: [], defaultPath: pages[0].path }
     }
     const tabs = documentTabs.reduce((acc, item) => {
       acc[item.name] = item
