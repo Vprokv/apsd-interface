3cd5fcab296b2bb95dcc8ad7d854e43cb5082475
mac os <forinventorwork@mail.ru>
2022-12-14 10:49:32 +0500

    APSD-641

--- src/Pages/Main/Components/SideBar/Components/CreateDocumentWindow/style.js
+++ src/Pages/Main/Components/SideBar/Components/CreateDocumentWindow/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Icon from '@Components/Components/Icon'
+import ModalWindowWrapper from '@/Components/ModalWindow'
 
 export const DocumentTypesContainer = styled.div`
   width: 37.8%;
@@ -9,4 +10,10 @@ export const DocumentTypesContainer = styled.div`
 export const DocumentIcon = styled(Icon)`
   margin-right: 0.75rem;
   color: ${({ selected }) => (selected ? 'var(--blue-1)' : '#DDE2FF')};
+`
+
+export const SmallSizeModalWindow = styled(ModalWindowWrapper)`
+  width: 35.6%;
+  height: 72.65%;
+  margin: auto;
 `
