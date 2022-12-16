8231487e68a91b97db7fc97f6c0f80903f522ccc
mac os <forinventorwork@mail.ru>
2022-12-16 14:16:57 +0500

    APSD-655 and disable button "close all tabs"

--- src/Pages/Main/Components/CloseAllTabsButton/index.js
+++ src/Pages/Main/Components/CloseAllTabsButton/index.js
@@ -11,7 +11,7 @@ const CloseAllTabButton = () => {
       active={true}
       className="rounded-md flex items-center py-1 px-1.5 font-size-12 justify-center mr-1 whitespace-nowrap"
       title={'Закрыть все окна'}
-      onClick={onCloseAllTab}
+      // onClick={onCloseAllTab}
     >
       <span className="truncate">{'Закрыть все окна'}</span>
     </Container>
