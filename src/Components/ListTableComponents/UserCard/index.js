6a54d9f33afa7612cb70db890ca61282e2c1bb26
mac os <forinventorwork@mail.ru>
2022-12-28 10:48:40 +0500

    APSD-732

--- src/Components/ListTableComponents/UserCard/index.js
+++ src/Components/ListTableComponents/UserCard/index.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types'
 import { UserCircle } from './styles'
 import colorFromString from '@Components/Utils/colorFromString'
 
-const UserCard = ({ name = '', fio = '', position, avatar } = {}) => {
+const UserCard = ({
+  name = '',
+  position,
+  avatar,
+  lastName,
+  middleName,
+} = {}) => {
+  const fio = useMemo(
+    () => `${lastName} ${name} ${middleName}`,
+    [lastName, middleName, name],
+  )
+
   const bg = useMemo(() => {
     return typeof fio === 'string' && colorFromString(fio, 30, 80)
   }, [fio])
@@ -26,15 +37,20 @@ const UserCard = ({ name = '', fio = '', position, avatar } = {}) => {
 }
 
 UserCard.propTypes = {
-  value: PropTypes.shape({
-    name: PropTypes.string,
-    surname: PropTypes.string.isRequired,
-    secondName: PropTypes.string,
-    position: PropTypes.string,
-  }),
+  name: PropTypes.string,
+  lastName: PropTypes.string,
+  middleName: PropTypes.string,
+  position: PropTypes.string,
+  avatar: PropTypes.string,
 }
 
-UserCard.defaultProps = {}
+UserCard.defaultProps = {
+  name: '',
+  lastName: '',
+  middleName: '',
+  position: '',
+  avatar: '',
+}
 
 export default UserCard
 
