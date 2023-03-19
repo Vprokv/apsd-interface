e71f55c9fe49f8e2d411447cb5eb03a494174747
mac os <forinventorwork@mail.ru>
2023-03-19 16:46:49 +0500

    fix api

--- src/api.js
+++ src/api.js
@@ -4,17 +4,20 @@ import axios from 'axios'
 // const API_URL = 'http://192.168.42.105/'
 
 //stage
-// export const API_URL = 'http://10.20.56.47/'
+export const API_URL = 'http://10.20.56.47/'
 
 //production
 // export const API_URL = 'https://psd.moesk.ru'
 
-// config for master branch
-export const API_URL =
-  process.env.NODE_ENV !== 'production'
-    ? process.env.REACT_APP_API_BASE_URL || 'http://192.168.42.105/'
-    : window.location.origin
+//LKP
+// export const API_URL = 'http://10.20.56.61/'
 
+// config for master branch
+// export const API_URL =
+//   process.env.NODE_ENV !== 'production'
+//     ? process.env.REACT_APP_API_BASE_URL || 'http://10.20.56.47/'
+//     : window.location.origin
+//
 axios.defaults.baseURL = API_URL
 
 export default function ({ token, ...apiParams }) {
