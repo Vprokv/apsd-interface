398805bb91e42a45a92ec5b8d46ab41e2e5fd290
mac os <forinventorwork@mail.ru>
2023-05-05 15:05:15 +0500

    APSD-1220 FE: Сделать форму вывода уведомлений

--- src/Pages/Notification/Components/DocumentTypeComponent/index.js
+++ src/Pages/Notification/Components/DocumentTypeComponent/index.js
@@ -1,16 +1,13 @@
 import React, { useMemo } from 'react'
 import dayjs from 'dayjs'
-import {
-  DEFAULT_DATE_FORMAT,
-  PRESENT_DATE_FORMAT,
-} from '@/contants'
+import { DEFAULT_DATE_FORMAT, PRESENT_DATE_FORMAT } from '@/contants'
 import Icon from '@Components/Components/Icon'
 import colorFromString from '@Components/Utils/colorFromString'
 import { VolumeStatus } from '@/Components/ListTableComponents/VolumeState/styles'
 import documentIcon from '@/Icons/documentIcon'
 
 const DocumentTypeComponent = ({
-  ParentValue: { documentRegNumber, documentTypeLabel, documentRegDate },
+  ParentValue: { documentRegNumber, documentTypeLabel, documentRegDate, read },
 }) => {
   const color = useMemo(() => {
     return colorFromString(documentTypeLabel, 100, 35)
@@ -27,28 +24,37 @@ const DocumentTypeComponent = ({
   )
 
   return (
-    <div className="flex flex-col w-full h-full items-start ">
-      <VolumeStatus
-        className="font-size-12 mb-1 font-medium px-1 py-0.5 rounded-md w-fit"
-        color={color}
-        bg={bg}
-        dangerouslySetInnerHTML={{
-          __html: useMemo(
-            () => documentTypeLabel.replaceAll('-', '&#x2011'),
-            [documentTypeLabel],
-          ),
-        }}
-      />
-      <div className="flex items-center font-size-12 flex-col ">
-        <div className="flex  justify-center mr-2 h-full">
-          <Icon
-            icon={documentIcon}
-            size={14}
-            className="mr-1 color-text-secondary"
-          />
-          <div className={'break-all'}>{`${
-            documentRegNumber ?? 'б/н'
-          } от ${date} `}</div>
+    <div className="flex h-full items-center">
+      <div
+        className={`h-full mr-2  ${
+          read ? 'color-white' : ' bg-blue-1 color-blue-1'
+        }`}
+      >
+        .
+      </div>
+      <div className="flex flex-col w-full h-full items-start ">
+        <VolumeStatus
+          className="font-size-12 mb-1 font-medium px-1 py-0.5 rounded-md w-fit"
+          color={color}
+          bg={bg}
+          dangerouslySetInnerHTML={{
+            __html: useMemo(
+              () => documentTypeLabel.replaceAll('-', '&#x2011'),
+              [documentTypeLabel],
+            ),
+          }}
+        />
+        <div className="flex items-center font-size-12 flex-col ">
+          <div className="flex  justify-center mr-2 h-full">
+            <Icon
+              icon={documentIcon}
+              size={14}
+              className="mr-1 color-text-secondary"
+            />
+            <div className={'break-all'}>{`${
+              documentRegNumber ?? 'б/н'
+            } от ${date} `}</div>
+          </div>
         </div>
       </div>
     </div>
