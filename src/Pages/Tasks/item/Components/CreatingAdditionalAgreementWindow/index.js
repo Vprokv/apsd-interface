80744c5aed2826e817ccb9a63ba72b5b69802e8d
mac os <forinventorwork@mail.ru>
2023-05-05 10:47:11 +0500

    APSD-1260 Наслоение окна 'Создание дополнительного согласования' на окно 'Добавление сотрудника'

--- src/Pages/Tasks/item/Components/CreatingAdditionalAgreementWindow/index.js
+++ src/Pages/Tasks/item/Components/CreatingAdditionalAgreementWindow/index.js
@@ -5,7 +5,7 @@ import Button from '@/Components/Button'
 import UserSelect from '@/Components/Inputs/UserSelect'
 import InputComponent from '@Components/Components/Inputs/Input'
 import Form from '@Components/Components/Forms'
-import { ApiContext, ITEM_TASK } from '@/contants'
+import { ApiContext, ITEM_TASK, TASK_ITEM_APPROVAL_SHEET } from '@/contants'
 import DefaultWrapper from '@/Components/Fields/DefaultWrapper'
 import { URL_APPROVAL_SHEET_CREATE_ADDITIONAL_AGREEMENT } from '../../../../../ApiList'
 import { DocumentIdContext } from '@/Pages/Tasks/item/constants'
@@ -17,6 +17,8 @@ import {
   useOpenNotification,
 } from '@/Components/Notificator'
 import { defaultFunctionsMap } from '@/Components/Notificator/constants'
+import UnderButtons from '@/Components/Inputs/UnderButtons'
+import { updateTabChildrenStates } from '@/Utils/TabStateUpdaters'
 
 const rules = {}
 
@@ -37,6 +39,7 @@ const CreatingAdditionalAgreementWindow = ({ onClose }) => {
   const [values, setValues] = useState({})
   const { onCloseTab } = useContext(TabStateManipulation)
   const { currentTabIndex } = useContext(CurrentTabContext)
+  const updateCurrentTabChildrenStates = updateTabChildrenStates()
   const closeCurrenTab = useCallback(
     () => onCloseTab(currentTabIndex),
     [onCloseTab, currentTabIndex],
@@ -70,15 +73,21 @@ const CreatingAdditionalAgreementWindow = ({ onClose }) => {
 
   const onSave = useCallback(async () => {
     try {
-      await api.post(URL_APPROVAL_SHEET_CREATE_ADDITIONAL_AGREEMENT, {
-        ...values,
-        parentPerformerId: approverId,
-        documentType,
-        documentId,
-      })
+      const { status } = await api.post(
+        URL_APPROVAL_SHEET_CREATE_ADDITIONAL_AGREEMENT,
+        {
+          ...values,
+          parentPerformerId: approverId,
+          documentType,
+          documentId,
+        },
+      )
       getNotification(customMessagesFuncMap[status]())
+      updateCurrentTabChildrenStates([TASK_ITEM_APPROVAL_SHEET], {
+        loading: false,
+        fetched: false,
+      })
       onClose()
-      closeCurrenTab()
     } catch (e) {
       const { response: { status, data } = {} } = e
       getNotification(customMessagesFuncMap[status](data))
@@ -90,8 +99,8 @@ const CreatingAdditionalAgreementWindow = ({ onClose }) => {
     documentType,
     documentId,
     getNotification,
+    updateCurrentTabChildrenStates,
     onClose,
-    closeCurrenTab,
   ])
   return (
     <div className="flex flex-col overflow-hidden h-full">
@@ -103,19 +112,14 @@ const CreatingAdditionalAgreementWindow = ({ onClose }) => {
         fields={fieldMap}
         rules={rules}
       />
+
       <div className="flex items-center justify-end mt-auto mt-auto">
-        <Button
-          className="bg-light-gray flex items-center w-60 rounded-lg mr-4 justify-center font-weight-normal"
-          onClick={onClose}
-        >
-          Закрыть
-        </Button>
-        <Button
-          className="text-white bg-blue-1 flex items-center w-60 rounded-lg justify-center font-weight-normal"
-          onClick={onSave}
-        >
-          Сохранить
-        </Button>
+        <UnderButtons
+          leftFunc={onClose}
+          rightFunc={onSave}
+          leftLabel={'Закрыть'}
+          rightLabel={'Сохранить'}
+        />
       </div>
     </div>
   )
