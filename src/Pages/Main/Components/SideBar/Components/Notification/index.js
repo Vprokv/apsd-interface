ab62e27d497b907c3c9ed0b4333a4a9569d8612e
mac os <forinventorwork@mail.ru>
2023-05-08 15:12:35 +0500

    APSD-1295 Получение списка кол-ва уведомлений по пользователю

--- src/Pages/Main/Components/SideBar/Components/Notification/index.js
+++ src/Pages/Main/Components/SideBar/Components/Notification/index.js
@@ -4,17 +4,18 @@ import { NOTIFICATION_PATH } from '@/routePaths'
 import { NavigationHeaderIcon } from '@/Pages/Main/Components/SideBar/style'
 import notificationIcon from '@/Icons/notificationIcon'
 import { ApiContext } from '@/contants'
+import { URL_SUBSCRIPTION_USER_TOTAL } from '@/ApiList'
 
 const Notification = ({ onOpenNewTab }) => {
   const api = useContext(ApiContext)
   const [notification, setNotification] = useState()
 
-  // useEffect(() => {
-  //   ;(async () => {
-  //     const { data } = await api.post(URL_USER_PASSWORD_RULES, { token })
-  //     setNotification(data)
-  //   })()
-  // }, [api, token])
+  useEffect(() => {
+    ;(async () => {
+      const { data } = await api.post(URL_SUBSCRIPTION_USER_TOTAL)
+      setNotification(data)
+    })()
+  }, [api])
 
   return (
     <div>
@@ -30,7 +31,9 @@ const Notification = ({ onOpenNewTab }) => {
         <span className="font-size-12 mr-auto font-medium">Уведомления</span>
 
         <span>
-          <span className="font-medium color-blue-1">{notification}</span>
+          <span className="font-medium color-blue-1 font-size-12">
+            {notification}
+          </span>
         </span>
       </button>
     </div>
