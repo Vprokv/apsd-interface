a6c8981d2fe8b818f185b17707c9462e8f6ef30f
mac os <forinventorwork@mail.ru>
2023-05-15 13:15:30 +0500

    APSD-1319 Добавить цвета статусов согласующихся

--- src/Pages/Tasks/item/Pages/ApprovalSheet/Components/DocumentState/index.js
+++ src/Pages/Tasks/item/Pages/ApprovalSheet/Components/DocumentState/index.js
@@ -12,30 +12,32 @@ const revoked = 'Отозвано'
 const accepted_for_execution = 'Принято к исполнению'
 const rejected = 'Отклонено'
 const approved = 'Утверждено'
+const saw_with_remarks = 'Рассмотрено с замечаниями'
+const saw = 'Рассмотрено'
 
 const colors = {
   [notSent]: 'bg-text-secondary',
   [approve]: 'bg-green',
-  [notApprove]: 'bg-red',
+  [notApprove]: 'bg-color-red',
   [sent_out]: 'bg-blue-2',
   [idle]: 'bg-text-secondary',
   [agreed_with_comments]: 'bg-color-yellow',
-  [revoked]: 'bg-red',
+  [revoked]: 'bg-color-red',
   [accepted_for_execution]: 'bg-blue-1',
-  [rejected]: 'bg-red',
+  [rejected]: 'bg-color-red',
   [approved]: 'bg-green',
+  [saw_with_remarks]: 'bg-color-red',
+  [saw]: 'bg-green',
 }
 
-const VolumeStatus = ({ value, className }) => {
-  return (
-    <div
-      className={`${className} word-wrap-anywhere font-size-14 flex items-center justify-start px-20`}
-    >
-      <StatusDot className={`mr-2 ${colors[value]}`} />
-      {value}
-    </div>
-  )
-}
+const VolumeStatus = ({ value, className }) => (
+  <div
+    className={`${className} word-wrap-anywhere font-size-14 flex items-center justify-start px-20`}
+  >
+    <StatusDot className={`mr-2 ${colors[value]}`} />
+    {value}
+  </div>
+)
 
 VolumeStatus.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
