77eeb7b1c19695d40bb97d5d5e00ef3992a3a00a
mac os <forinventorwork@mail.ru>
2023-05-31 10:29:58 +0500

    APSD-1422 После каждого поиска требуется обновление страницы

--- src/Pages/Search/Pages/DocumentSearch/index.js
+++ src/Pages/Search/Pages/DocumentSearch/index.js
@@ -281,7 +281,7 @@ const DocumentSearch = ({
 
   const onSearch = useCallback(async () => {
     setRenderTable(true)
-  }, [api, defaultOperators, filter, getNotification, setSearchState])
+  }, [])
 
   const onExportToExcel = useCallback(async () => {
     const { type, ...filters } = filter
@@ -333,6 +333,11 @@ const DocumentSearch = ({
     return Object.keys(keys).length === 0
   }, [filter])
 
+  const onCloseTable = useCallback(() => {
+    setRenderTable(false)
+    setSearchState([])
+  }, [setSearchState])
+
   return (
     <ExportContext.Provider value={'asas'}>
       <div className="flex flex-col w-full p-4 overflow-hidden">
@@ -343,7 +348,7 @@ const DocumentSearch = ({
             filter={filter}
             onExportToExcel={onExportToExcel}
           >
-            {children(() => setRenderTable(false), onExportToExcel)}
+            {children(() => onCloseTable(), onExportToExcel)}
           </TableSearch>
         ) : (
           <div className="flex overflow-hidden">
@@ -435,7 +440,7 @@ const TableSearch = ({
     const { limit, offset } = paginationState
     const queryItems = Object.entries(filters).reduce(
       (acc, [key, { value, operator }]) => {
-        value.length &&
+        value?.length &&
           acc.push({
             attr: key,
             operator: operator || defaultOperators[key],
