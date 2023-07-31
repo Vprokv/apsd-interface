1cb9adf62bb653e7526d14959b8c858f155f0614
mac os <forinventorwork@mail.ru>
2023-07-31 10:43:36 +0500

    APSD-1656 Убрать время в полях с датами на вкладке "Состав титула"

--- src/Pages/Tasks/item/Pages/Contain/Components/DateCell/index.js
+++ src/Pages/Tasks/item/Pages/Contain/Components/DateCell/index.js
@@ -3,29 +3,42 @@ import Icon from '@Components/Components/Icon'
 import calendarIcon from '@/Icons/calendarIcon'
 import clockIcon from '@/Icons/clockIcon'
 import PropTypes from 'prop-types'
+import dayjs from 'dayjs'
+import { DEFAULT_DATE_FORMAT, PRESENT_DATE_FORMAT } from '@/contants'
 
 const DateCell = ({ real, plan }) => {
   const hot = useMemo(() => real > plan, [plan, real])
+
+  const shortReal = useMemo(
+    () => real && dayjs(real, DEFAULT_DATE_FORMAT).format(PRESENT_DATE_FORMAT),
+    [real],
+  )
+
+  const shortPlan = useMemo(
+    () => plan && dayjs(plan, DEFAULT_DATE_FORMAT).format(PRESENT_DATE_FORMAT),
+    [plan],
+  )
 
   return (
     <div>
       <div className="flex mb-2 font-size-12">
         <Icon icon={calendarIcon} className="mr-2 color-text-secondary" />
-        {plan}
+        {shortPlan}
       </div>
       <div className="flex font-size-12">
         <Icon
           icon={clockIcon}
           className={`${hot ? 'color-red' : ''} mr-2 color-text-secondary`}
         />
-        {real}
+        {shortReal}
       </div>
     </div>
   )
 }
 
 DateCell.propTypes = {
-  value: PropTypes.string,
+  real: PropTypes.string,
+  plan: PropTypes.string,
 }
 
 export default DateCell
