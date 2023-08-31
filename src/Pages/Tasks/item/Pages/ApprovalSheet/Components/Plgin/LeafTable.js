db9d86f55456e6f31c018c49f457a028ac6275dd
mac os <forinventorwork@mail.ru>
2023-08-31 09:59:04 +0500

    APSD-1677 FE: Изменить модель получаемых данных на вкладке ЖЦ

--- src/Pages/Tasks/item/Pages/ApprovalSheet/Components/Plgin/LeafTable.js
+++ src/Pages/Tasks/item/Pages/ApprovalSheet/Components/Plgin/LeafTable.js
@@ -20,19 +20,42 @@ const RowComponent = ({ node, RowC }) => {
     dueDate,
     report,
     additionalStage,
+    approvers,
+    dssApproverFio,
+    dssApproverPosition,
+    dssStatus,
+    dsdtDecision,
+    dsdtDueDate,
+    dsdtAvatar,
   } = node
+
+  const [isDisplayed, setShow] = useState(true)
+
+  const toggleDisplayedFlag = useCallback(() => setShow((s) => !s), [])
+
+  const renderedEntities = useMemo(
+    () =>
+      approvers?.map((approver, i) => {
+        return <RowC key={i} node={approver} RowC={RowC} />
+      }),
+    [RowC, approvers],
+  )
 
   return (
     <Row>
       <RowGrid className="h-full items-center ml-2">
-        <UserCard fio={approverFio} position={approverPosition} />
+        <UserCard
+          fio={dssApproverFio}
+          position={dssApproverPosition}
+          avatar={dsdtAvatar}
+        />
         <DateCell
-          hot={decisionDate && dueDate ? decisionDate < dueDate : false}
-          plan={decisionDate}
-          fact={dueDate}
+          hot={dsdtDecision && dsdtDueDate ? dsdtDecision < dsdtDueDate : false}
+          plan={dsdtDecision}
+          fact={dsdtDueDate}
           className=""
         />
-        <DocumentState value={status} className="" />
+        <DocumentState value={dssStatus} className="" />
         <HideAndShowText
           className="font-size-14 break-all flex items-center m-width max-w-xs"
           value={report?.dssReportText}
@@ -43,7 +66,29 @@ const RowComponent = ({ node, RowC }) => {
           {...report}
         />
       </RowGrid>
-      <AdditionalStage {...additionalStage} Component={RowC} />
+      {approvers && (
+        <div className="ml-6">
+          <LevelStage onClick={toggleDisplayedFlag}>
+            <button
+              className="pl-2"
+              type="button"
+              onClick={toggleDisplayedFlag}
+            >
+              <Icon
+                icon={angleIcon}
+                size={10}
+                className={`color-text-secondary ${
+                  isDisplayed ? '' : 'rotate-180'
+                }`}
+              />
+            </button>
+            <div className="font-size-12 ml-2">
+              {'Дополнительное согласование'}
+            </div>
+          </LevelStage>
+          <div className="">{isDisplayed && renderedEntities}</div>
+        </div>
+      )}
     </Row>
   )
 }
@@ -58,3 +103,24 @@ RowComponent.defaultProps = {
 }
 
 export default RowComponent
+//Todo вернуть в ROW
+{/*<RowGrid className="h-full items-center ml-2">*/}
+{/*  <UserCard fio={approverFio} position={approverPosition} />*/}
+{/*  <DateCell*/}
+{/*    hot={decisionDate && dueDate ? decisionDate < dueDate : false}*/}
+{/*    plan={decisionDate}*/}
+{/*    fact={dueDate}*/}
+{/*    className=""*/}
+{/*  />*/}
+{/*  <DocumentState value={status} className="" />*/}
+{/*  <HideAndShowText*/}
+{/*    className="font-size-14 break-all flex items-center m-width max-w-xs"*/}
+{/*    value={report?.dssReportText}*/}
+{/*    numberOfCharactersDisplayed={100}*/}
+{/*    buttonComponent={() => (*/}
+{/*      <Button className="color-text-secondary">...</Button>*/}
+{/*    )}*/}
+{/*    {...report}*/}
+{/*  />*/}
+{/*</RowGrid>*/}
+{/*<AdditionalStage {...additionalStage} Component={RowC} />*/}
