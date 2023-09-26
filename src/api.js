d191f11265a801b2b48033801c8976928510dc5c
mac os <forinventorwork@mail.ru>
2023-09-26 13:21:18 +0500

    add default value for checkbox in report only_original item

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // const API_URL = 'http://192.168.42.105/'
 
 //stage
-export const API_URL = 'http://10.20.56.47/'
+// export const API_URL = 'http://10.20.56.47/'
 // export const API_URL = 'http://10.20.56.50/'
 
 //production
@@ -14,11 +14,11 @@ export const API_URL = 'http://10.20.56.47/'
 // export const API_URL = 'http://10.20.56.61/'
 
 // config for master branch
-// export const API_URL =
-//   process.env.NODE_ENV !== 'production'
-//     ? process.env.REACT_APP_API_BASE_URL || 'http://10.20.56.47/'
-//     : window.location.origin
-//
+export const API_URL =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.REACT_APP_API_BASE_URL || 'http://10.20.56.47/'
+    : window.location.origin
+
 axios.defaults.baseURL = API_URL
 
 export default function ({ token, ...apiParams }) {
