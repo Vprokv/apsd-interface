81c8554f4a40ad18f872d022da0085d1e11df82e
mac os <forinventorwork@mail.ru>
2023-09-29 13:15:30 +0500

    up timeout

--- src/api.js
+++ src/api.js
@@ -24,7 +24,7 @@ axios.defaults.baseURL = API_URL
 export default function ({ token, ...apiParams }) {
   const api = axios.create({
     baseURL: API_URL,
-    timeout: 120000,
+    timeout: 120000000, //todo поднят по просьбе. Уточнить необходимость
     ...apiParams,
   })
 
