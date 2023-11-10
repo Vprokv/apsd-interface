a45d6dc80143b5d7c8ef5d43ecf9b9ce453c52b9
mac os <forinventorwork@mail.ru>
2023-11-10 11:11:11 +0500

    APSD-1897 Ошибки в заполнении полей на вкладке "Состав титула"

--- src/Pages/Tasks/item/Pages/Contain/Components/ResultCell/index.js
+++ src/Pages/Tasks/item/Pages/Contain/Components/ResultCell/index.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
+import { useCallback, useMemo } from 'react'
 
 const ResultCell = ({
+  ParentValue,
   ParentValue: {
     statusResult,
     tomStageResult,
@@ -10,14 +12,26 @@ const ResultCell = ({
   } = {},
   className = 'min-h-10',
 }) => {
+  // console.log(delayDevelopmentDay, 'delayyDevelopmentDay')
+  console.log(
+    delayApprovalDay,
+    'delayApprovalDay',
+    (delayDevelopmentDay.length || delayApprovalDay.length) > 0
+      ? 'bg-light-red'
+      : '',
+  )
+
+  const colorMemo = useMemo(
+    () =>
+      (delayDevelopmentDay.length || delayApprovalDay.length) > 0
+        ? 'bg-light-red'
+        : '',
+    [delayApprovalDay, delayDevelopmentDay],
+  )
+  console.log(colorMemo, 'colorMemo')
   return (
     <div
-      className={`${className} flex flex-col word-wrap-anywhere font-size-12 break-all ${
-        (delayDevelopmentDay || delayApprovalDay) &&
-        (delayDevelopmentDay || delayApprovalDay) > 0
-          ? 'bg-light-red'
-          : ''
-      }`}
+      className={`${className} h-full w-full flex flex-col word-wrap-anywhere font-size-12 break-all ${colorMemo}`}
     >
       {statusResult && <div>Состояние: {statusResult}</div>}
       {tomStageResult && <div>Этап: {tomStageResult}</div>}
