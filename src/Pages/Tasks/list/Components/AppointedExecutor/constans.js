8efb1a31384a1e469d9b1f13e1834a70a0214425
mac os <forinventorwork@mail.ru>
2023-11-20 17:08:05 +0500

    APSD-1913 Нет кружочка в колонке "Назначенный исполнитель" у сотрудника, завершившего доп. согласование

--- src/Pages/Tasks/list/Components/AppointedExecutor/constans.js
+++ src/Pages/Tasks/list/Components/AppointedExecutor/constans.js
@@ -3,6 +3,7 @@ const NEW_STATUS = 'new'
 const ACQUIRED_STATUS = 'acquired'
 const CONSIDERED_WITH_REMARKS_STATUS = 'сonsidered_with_remarks'
 const CONSIDERED_STATUS = 'сonsidered'
+const APPROVED_STATUS = 'approved'
 const NOT_APPROVED_STATUS = 'not_approved'
 const RECALLED_STATUS = 'recalled'
 
@@ -12,6 +13,7 @@ export const StatusMap = {
   [ACQUIRED_STATUS]: 'Принято к исполнению',
   [CONSIDERED_WITH_REMARKS_STATUS]: 'Рассмотрено с замечаниями',
   [CONSIDERED_STATUS]: 'Рассмотренно',
+  [APPROVED_STATUS]: 'Согласовано',
   [NOT_APPROVED_STATUS]: 'Не согласовано',
   [RECALLED_STATUS]: 'Отозвано',
 }
@@ -22,6 +24,7 @@ export const colorsMap = {
   [ACQUIRED_STATUS]: 'bg-blue-1',
   [CONSIDERED_WITH_REMARKS_STATUS]: 'bg-blue-1',
   [CONSIDERED_STATUS]: 'bg-green',
+  [APPROVED_STATUS]: 'bg-green',
   [NOT_APPROVED_STATUS]: 'bg-color-red',
   [RECALLED_STATUS]: 'bg-color-red',
 }
