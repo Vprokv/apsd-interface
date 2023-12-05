9da0210eb9bf3ba97467da81b7cf72eef0fc3a51
mac os <forinventorwork@mail.ru>
2023-12-05 20:24:57 +0500

    APSD-1931 Изменить формат отправки параметра типа "Дата" для всех отчетов

--- src/Pages/Rporting/Parser/addDataPickerAdapter.js
+++ src/Pages/Rporting/Parser/addDataPickerAdapter.js
@@ -2,20 +2,19 @@ import { useCallback, useMemo } from 'react'
 
 // value DataPicker Разбито в стейте на 2 поля с соовествующими префиксами
 const addDataPickerAdapter = () => (fieldState) => () => {
-  fieldState.hooks.push(({ formPayload, id, onInput }) => {
+  fieldState.hooks.push(({ id, onInput, value = {} }) => {
     const beforeId = `${id}_before`
     const afterId = `${id}_after`
     // извлекаем значение из формы
-    const { [beforeId]: before, [afterId]: after } = formPayload
+    const { [beforeId]: before, [afterId]: after } = value
     return {
       range: true, // делаем датапикер всегда range
       onInput: useCallback(
         ([before, after]) => {
-          // всегда отправляем в форму оба значения. т.к. не знаем какое поменялось
-          onInput(before, beforeId)
-          onInput(after, afterId)
+          //отправляем объект с динамическими ключами
+          onInput({ [beforeId]: before, [afterId]: after }, id)
         },
-        [afterId, beforeId, onInput],
+        [afterId, beforeId, id, onInput],
       ),
       value: useMemo(() => [before, after], [before, after]), // мемоизируем значения для поля
     }
