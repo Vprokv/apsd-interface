e9cc9acf23e7fd958c2e26fa1aeb69ccf38af79e
Denis <y.prokopchuk@id-mt.ru>
2024-01-26 15:53:45 +0500

    APSD-1860

--- src/Pages/Rporting/Components/ReportingSelect.js
+++ src/Pages/Rporting/Components/ReportingSelect.js
@@ -5,14 +5,26 @@ import {
   useLoadableSelect,
   useResolveView,
 } from '@Components/Components/Inputs/Select'
+import styled from 'styled-components'
+import { BasicMultipleOptionContainer } from '../../../components_ocean/Components/Inputs/Select'
 
 const AutoLoadableSelectPlugins = [
   useLoadableSelect,
   useResolveView,
   UseAutoLoadable,
 ]
+
+export const StyledOptionContainer = styled(BasicMultipleOptionContainer)`
+  --multipleSelectedOptionContainer: 380px;
+`
+
 const ReportingSelect = forwardRef((props, ref) => (
-  <Select hooks={AutoLoadableSelectPlugins} {...props} ref={ref} />
+  <Select
+    hooks={AutoLoadableSelectPlugins}
+    {...props}
+    multipleOptionsComponent={StyledOptionContainer}
+    ref={ref}
+  />
 ))
 
 export default ReportingSelect
