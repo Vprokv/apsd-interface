f3e517d343fddb04e6ce2d9e37b4d2c6013247ad
mac os <forinventorwork@mail.ru>
2024-02-20 15:30:30 +0500

    APSD-2025 АПСД. Предпросмотр контента на весь экран (режим презентации)

--- src/Components/PreviewContentWindow/Components/PdfReaderComponent.js
+++ src/Components/PreviewContentWindow/Components/PdfReaderComponent.js
@@ -4,11 +4,79 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 import ru_RU from '@react-pdf-viewer/locales/lib/ru_RU.json'
 
 const PdfReaderComponent = ({ url }) => {
-  const newPlugin = defaultLayoutPlugin()
+  const renderToolbar = (Toolbar) => {
+    return (
+      <Toolbar>
+        {(slots) => {
+          const {
+            CurrentPageInput,
+            EnterFullScreen,
+            GoToNextPage,
+            GoToPreviousPage,
+            NumberOfPages,
+            Print,
+            ShowSearchPopover,
+            Zoom,
+            ZoomIn,
+            ZoomOut,
+            SwitchTheme,
+          } = slots
+          return (
+            <div className="flex items-center w-full">
+              <div className="" style={{ padding: '0px 2px' }}>
+                <ShowSearchPopover />
+              </div>
+              <div style={{ padding: '0px 2px' }}>
+                <ZoomOut />
+              </div>
+              <div style={{ padding: '0px 2px' }}>
+                <Zoom />
+              </div>
+              <div style={{ padding: '0px 2px' }}>
+                <ZoomIn />
+              </div>
+              <div className="flex items-center ml-auto">
+                <div style={{ padding: '0px 2px' }}>
+                  <GoToPreviousPage />
+                </div>
+                <CurrentPageInput />
+                <div className="bg-secondary py-2 h-full px-2">
+                  /
+                  <NumberOfPages />
+                </div>
+                <div style={{ padding: '0px 2px' }}>
+                  <GoToNextPage />
+                </div>
+              </div>
+
+              <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                <EnterFullScreen />
+              </div>
+              <div style={{ padding: '0px 2px' }}>
+                <Print />
+              </div>
+              <div style={{ padding: '0px 2px' }}>
+                <SwitchTheme />
+              </div>
+            </div>
+          )
+        }}
+      </Toolbar>
+    )
+  }
+
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+    renderToolbar,
+  })
 
   return (
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <Viewer fileUrl={url} plugins={[newPlugin]} localization={ru_RU} />
+      <Viewer
+        fileUrl={url}
+        plugins={[defaultLayoutPluginInstance]}
+        localization={ru_RU}
+        theme="dark"
+      />
     </Worker>
   )
 }
