4910e839a5e5b91fe3015ddfc19a7246c7e7681b
Denis <y.prokopchuk@id-mt.ru>
2024-03-04 15:03:26 +0500

    APSD-2068

--- src/api.js
+++ src/api.js
@@ -1,30 +1,30 @@
 import axios from 'axios'
 
-//development
+// development
 // const API_URL = 'http://192.168.42.105/'
 
-//stage
-//export const API_URL = 'http://10.20.56.47/'
-//export const API_URL = 'http://10.20.56.50/'
+// stage
+// export const API_URL = 'http://10.20.56.47/'
+export const API_URL = 'http://10.20.56.50/'
 
 // production
 // export const API_URL = 'https://psd.moesk.ru'
 
-//LKP
+// LKP
 // export const API_URL = 'http://10.20.56.61/'
 
 // config for master branch
-export const API_URL =
+/* export const API_URL =
   process.env.NODE_ENV !== 'production'
     ? process.env.REACT_APP_API_BASE_URL || 'http://10.20.56.47/'
-    : window.location.origin
+    : window.location.origin*/
 
 axios.defaults.baseURL = API_URL
 
 export default function ({ token, ...apiParams }) {
   const api = axios.create({
     baseURL: API_URL,
-    timeout: 600000, //todo поднят по просьбе. Уточнить необходимость
+    timeout: 600000, // todo поднят по просьбе. Уточнить необходимость
     ...apiParams,
   })
 
